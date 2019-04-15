b37debf50f725b93879acc7808497c26f9af6448
heiligbasil <basilief@yahoo.com>
2019-04-15 12:41:26 -0700

    Added layout for search page

---
+++ app/src/main/java/com/lambdaschool/build_week3_simpsons_says/Search.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.build_week3_simpsons_says;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
