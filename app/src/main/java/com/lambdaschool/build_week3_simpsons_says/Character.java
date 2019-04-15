e9a2bac90e6ddfa790d29f9166257ce78f2ff1b8
heiligbasil <basilief@yahoo.com>
2019-04-15 16:38:22 -0700

    Added NetworkAdapter and POJO classes

---
+++ app/src/main/java/com/lambdaschool/build_week3_simpsons_says/Character.java
@@ -0,0 +1,6 @@
+package com.lambdaschool.build_week3_simpsons_says;
+
+public class Character {
+    private int id;
+    private String name;
+}
---
+++ app/src/main/java/com/lambdaschool/build_week3_simpsons_says/Quote.java
@@ -0,0 +1,6 @@
+package com.lambdaschool.build_week3_simpsons_says;
+
+public class Quote {
+    private int id;
+    private String quote;
+}
