2ee30e28177b7ae516dc749649e2003abd93c8b8
heiligbasil <basilief@yahoo.com>
2019-04-16 13:32:54 -0700

    Brought the Search Activity online as accessed from the Main Activity

--- app/src/main/java/com/lambdaschool/build_week3_simpsons_says/MainActivity.java
+++ app/src/main/java/com/lambdaschool/build_week3_simpsons_says/MainActivity.java
@@ -1,53 +1,43 @@
 package com.lambdaschool.build_week3_simpsons_says;
 
-import android.net.Uri;
-import android.os.Environment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity implements DetailsFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_main);
+        final Context context = this;
 
-        DataAccessObject dao = new DataAccessObject();
-        ArrayList<Quote> quoteArrayList = dao.getData();
-
-        RecyclerView recyclerView = findViewById(R.id.recycler_view_search_results);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        SearchListAdapter searchListAdapter = new SearchListAdapter(quoteArrayList);
-        recyclerView.setAdapter(searchListAdapter);
-        recyclerView.setHasFixedSize(true);
-
-    }
-
-    @Override
-    public void onFragmentInteraction(Quote quote) {
-
-/*
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction
-                .replace(R.id.fragment_container, new ItemListFragment())
-                .commit();
-*/
-
-        DetailsFragment detailsFragment = new DetailsFragment();
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("details", quote);
-        detailsFragment.setArguments(bundle);
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.layout_details, detailsFragment)
-                .addToBackStack(null)
-                .commit();
+        Button buttonSearch = findViewById(R.id.button_search_activity);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Search.class);
+                startActivity(intent);
+            }
+        });
+        Button buttonGenerate = findViewById(R.id.button_generate_activity);
+        buttonGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Generate.class);
+                startActivity(intent);
+            }
+        });
+        Button buttonFavorites = findViewById(R.id.button_favorites_activity);
+        buttonFavorites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Favorites.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/lambdaschool/build_week3_simpsons_says/Search.java
+++ app/src/main/java/com/lambdaschool/build_week3_simpsons_says/Search.java
@@ -1,13 +1,64 @@
 package com.lambdaschool.build_week3_simpsons_says;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class Search extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Search extends AppCompatActivity implements DetailsFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        final Context context = this;
+
+        Button buttonSearch = findViewById(R.id.button_search_search);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DataAccessObject dao = new DataAccessObject();
+                ArrayList<Quote> quoteArrayList = dao.getData();
+                DetailsFragment.OnFragmentInteractionListener interactionListener = (DetailsFragment.OnFragmentInteractionListener) context; //TODO: Delete this and get proper handle
+                RecyclerView recyclerView = findViewById(R.id.recycler_view_search_results);
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                SearchListAdapter searchListAdapter = new SearchListAdapter(quoteArrayList, interactionListener);
+                recyclerView.setAdapter(searchListAdapter);
+                recyclerView.setHasFixedSize(true);
+            }
+        });
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Quote quote) {
+
+/*
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction
+                .replace(R.id.fragment_container, new ItemListFragment())
+                .commit();
+*/
+
+        DetailsFragment detailsFragment = new DetailsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("details", quote);
+        detailsFragment.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.layout_details, detailsFragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
